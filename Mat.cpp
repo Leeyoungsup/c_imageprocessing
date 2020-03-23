dd2e5403dd152b88ee0e2c1211ada3ef145fac8d
leeyoungsup <58593829+Leeyoungsup@users.noreply.github.com>
2020-03-23 19:19:23 +0900

    openCV_mat Class 20.03.22

--- Mat.cpp
+++ Mat.cpp
@@ -11,10 +11,17 @@ int main(){
     Mat mat1 = Mat::ones(3,3,CV_32FC1);//the matrix is 1-channel-integer matrix of ones
     Mat mat2 = Mat::eye(3,3,CV_32FC1);//the matrix is 1-channel-integer unit matrix
     Mat mat3 = Mat::zeros(3,3,CV_32FC1);//the matrix is 1-channel-integer matrix of zeros
-    float data[]={1,2,0,0,255,6,7,255,9};//list variable declaration
+    float data[]={1,0,1,0,1,0,1,0,1};//list variable declaration
     Mat mat4(3,3,CV_32FC1,data);//list matrixing
+    mat4.create(256,256,CV_8UC3);//assign new size and type to mat4
+    Mat mat5(4,4,CV_32FC1);//assign new size and type to mat5
+    mat4=Scalar(255,0,0);//Batch change of all element value to matrix
+    mat5.setTo(1.f);//Batch change of all element value to matrix
+    img1=imread("lena.png");//assign photo to img1
+    img2=img1;//shallow copy of img1 to img2
+    img1.copyTo(img5);//Deep copy of img1 to img5
     namedWindow("image");
-    imshow("image",mat4);
+    imshow("image",img5);
     waitKey(0);  
     return 0;
 }
